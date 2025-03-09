c4da772a7432b5c5f00b7aeb75ad36d9e99ec7a3
PendyV <saftigesau@gmail.com>
2025-03-09 22:19:08 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,6 +2,6 @@ local color = {r = 255, g = 57, b = 219} -- Color Config!
   
 CreateThread(function()
     if IsWaypointActive() then -- Check if Waypoint is active
-        ReplaceHudColourWithRgba(142,color.r,color.g,color.b,255) -- Change Waypoint Color
+        ReplaceHudColourWithRgba(142, color.r, color.g, color.b, 255) -- Change Waypoint Color
     end
 end)
